dbaed024103d57afab56d01f48cda8f2cc301d97
Dalibor Stanković <dargethlam@gmail.com>
2019-03-29 17:15:44 +0100

    lets start

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# current dir is ~/.vim/
+CURRENT_DIR='pwd'
+VUNDLE_DIR=$HOME/.vim/bundle
+
+echo "> setting up symlinks"
+ln -sf $CURRENT_DIR/vimconfig/vimrc $HOME/.vimrc
+
+echo "> install Vundle"
+if [ ! -e $VUNDLE_DIR/Vundle.vim ]; then
+    echo "Installing Vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR/Vundle.vim
+else
+    echo "Upgrade Vundle"
+    cd "$VUNDLE_DIR/Vundle.vim" && git pull origin master
+fi
+
+echo "> install/update Vundle plugins"
+currnet_system_shell=$SHELL
+export SHELL="/bin/sh"
+vim +PluginInstall! +PluginClean +qall
+export SHELL=$currnet_system_shell
+
+echo "> install snippets"
+cp -r $CURRENT_DIR/vimconfig/UltiSnips $HOME/.vim
+
+echo "> install dependency"
+sudo apt-get install -y silversearcher-ag
+npm install -g jslint jshint esctags
+pip install pylint
+
+echo "> compile YouCompleteMe"
